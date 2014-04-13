fb432a600fab53f868b9c2883cb2664e2a3a8a0f
xycoding <xyycoding@gmail.com>
2014-04-13 10:54:23 +0800

    comment

--- src/cn/sina/weibo/xycoding/checkj/CheckJ.java
+++ src/cn/sina/weibo/xycoding/checkj/CheckJ.java
@@ -3,7 +3,7 @@ package cn.sina.weibo.xycoding.checkj;
 public class CheckJ {
 	public static void main(String[] args) throws Exception {
 		System.out.println("trying to find conflict jars and classes, please wait.......");
-		System.out.println("suggest -XX:MaxPermSize more than 512M .......");
+		System.out.println("if the lib too many, suggest -XX:MaxPermSize more than 512M .......");
 		JFilesUtil fu = new JFilesUtil(args);
 		NameConflictCheck ccc = new NameConflictCheck(fu.getJarList());
 		ccc.printConfict();
